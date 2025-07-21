8533052f294a9197dac8667730ef2dc9cfa2564e
AshishBhardwaj1 <fireash70@gmail.com>
2025-07-21 12:14:52 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,14 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import path from "path";
-// import tailwindcss from "@tailwindcss/vite";
-
-// export default defineConfig(({ mode }) => ({
-//   plugins: [react(),tailwindcss()],
-//   base: mode === "production" ? "/portfolio/" : "/",
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), tailwindcss()],
-  base: "/", // Not "/portfolio/"
+
+export default defineConfig(() => ({
+  plugins: [react(),tailwindcss()],
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-
